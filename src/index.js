8cea84b1c75cddef5fbc2d85f1f786467847b540
Lokesh598 <ashawat.lokesh@gmail.com>
2022-02-08 19:45:17 +0530

    jsx practice-with-date

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom'
 let fName = "Lokesh";
 let lName = "singh";
-let year = 2019;
+// new Date().getFullYear()
+const currentDate = new Date();
+console.log(currentDate);
+const year = currentDate.getFullYear();
+// let year = 2019;
 ReactDom.render(
   <div>
     <p>my name {fName+" "+lName}</p>
