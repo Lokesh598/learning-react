57ca928ef218ab515e5af852ef0ac992ea3dfe0d
Lokesh598 <ashawat.lokesh@gmail.com>
2022-02-08 19:38:40 +0530

    jsx practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom'
+let fName = "Lokesh";
+let lName = "singh";
+let year = 2019;
+ReactDom.render(
+  <div>
+    <p>my name {fName+" "+lName}</p>
+    <p>copyright {year}</p>
+  </div>,
+  document.getElementById('root'))
+  
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
